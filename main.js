79ff1006f71937756a2e7e4f302edefad20bf358
SaiSameekshaVaishnavi <119114200+SaiSameekshaVaishnavi@users.noreply.github.com>
2024-06-25 14:56:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 var typed=new typed(".text",{
-    Strings:["Web Developer", "Frontend Developer"],
+    Strings:["Web Developer", "Software Developer", "Frontend Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
     loop:true
-});
+  });
+
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+function opentab(tabname)
+{
+       for(tablink of tablinks)
+       {
+           tablink.classList.remove("active-link");
+       }
+       for(tabcontent of tabcontents)
+       {
+           tablink.classList.remove("active-tab");
+       }
+}
